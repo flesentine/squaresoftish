142717030c2ca081546572848936a807fba0d750
flesentine <chrisgmcc@gmail.com>
2026-06-23 17:32:41 -0700

    Register battle scene

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import './style.css';
 import { GameScene } from './scenes/GameScene';
+import { BattleScene } from './scenes/BattleScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -16,7 +17,7 @@ const config: Phaser.Types.Core.GameConfig = {
       debug: false
     }
   },
-  scene: [GameScene],
+  scene: [GameScene, BattleScene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
